41f8c19527d9e0f2c9f15c9f921eac73467d26fd
Wan Zou <wanzou1126@gmail.com>
2020-01-12 13:00:14 +1100

    Implemented basic UI.

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Header from './Components/Header';
+import StartGameScreen from './screens/StartGameScreen';
+export default function App() {
+  return (
+    <View style={styles.screen}>
+      <Header title={'Guess a Number'} />
+      <StartGameScreen />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen:{
+    flex:1
+  }
+});
